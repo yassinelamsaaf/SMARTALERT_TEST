c2c0abb21edd48e8e2d09144086c4ff81d8ef015
yassine <lamsaafyassine20@gmail.com>
2025-07-05 19:54:52 +0100

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import { fetchSliderCriteria } from "./features/filters/sliderCriteriaSlice";
 import { fetchSourceCriteria } from "./features/filters/sourceCriteriaSlice";
 import { LanguageContext } from "./i18n/LanguageProvider";
 import { AlertListPage } from "./pages/alerts/index.jsx";
-import { AlertInfo } from "./pages/alerts/alertInfo.jsx"
+import { AlertInfo } from "./pages/alerts/alertInfo.jsx";
 import CarListPage2 from "./pages/car/car-list-v2";
 import CarSingleV1Dynamic from "./pages/car/car-single/indexNew";
 import Home_2 from "./pages/homes/home_2";
@@ -36,7 +36,6 @@ if (typeof window !== "undefined") {
   import("bootstrap");
 }
 
-
 import ActivateAccount from "./pages/others/activate";
 import PhoneVerification from "./components/phoneVerification";
 import SettingsPage from "./pages/settings";
@@ -45,27 +44,25 @@ import ChatbotFloatingButton from "./components/chatbot/ChatbotFloatingButton";
 import WishlistPage from "./pages/favoris/WishlistPage";
 import AnnoncePage from "./pages/annonce/AnnoncePage";
 
-
 function App() {
   const dispatch = useDispatch();
-  const {lang} = useContext(LanguageContext);
+  const { lang } = useContext(LanguageContext);
   useEffect(() => {
     dispatch(fetchBrandCriteria());
     dispatch(fetchCityCriteria());
     dispatch(fetchSourceCriteria());
     dispatch(fetchChipCriteria());
     dispatch(fetchSliderCriteria());
-    dispatch(fetchPromoCars())
+    dispatch(fetchPromoCars());
   }, [dispatch]);
 
-
   useEffect(() => {
-    window.fbAsyncInit = function() {
+    window.fbAsyncInit = function () {
       window.FB.init({
-        appId      : import.meta.env.VITE_FACEBOOK_APP_ID,
-        cookie     : true,
-        xfbml      : true,
-        version    : 'v19.0'
+        appId: import.meta.env.VITE_FACEBOOK_APP_ID,
+        cookie: true,
+        xfbml: true,
+        version: "v19.0",
       });
     };
     Aos.init({
@@ -77,41 +74,38 @@ function App() {
   return (
     <main>
       <Provider store={store}>
-        <BrowserRouter basename={"/smartalert"}>
-            <Routes>
-              <Route path="/" index element={<Home_2 />} />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" index element={<Home_2 />} />
 
-              <Route path="occasion" element={<CarListPage2 />} />
-              <Route path="neuve" element={<CarListPage2 />} />
+            <Route path="occasion" element={<CarListPage2 />} />
+            <Route path="neuve" element={<CarListPage2 />} />
 
-              <Route path="neuve/:id" element={<HotelSingleV1Dynamic />} />
-              <Route path="occasion/:id" element={<CarSingleV1Dynamic />} />
+            <Route path="neuve/:id" element={<HotelSingleV1Dynamic />} />
+            <Route path="occasion/:id" element={<CarSingleV1Dynamic />} />
 
-              <Route path="404" element={<NotFoundPage />} />
-              <Route path="*" element={<NotFoundPage />} />
+            <Route path="404" element={<NotFoundPage />} />
+            <Route path="*" element={<NotFoundPage />} />
 
-              <Route path="about" element={<About />} />
-              <Route path="terms" element={<Terms />} />
+            <Route path="about" element={<About />} />
+            <Route path="terms" element={<Terms />} />
 
-              <Route path="login" element={<LogIn />} />
-              <Route path="signup" element={<SignUp />} />
-              <Route path="PhoneVerification" element={<PhoneVerification/>}/> 
-              <Route path="reglages" element={<SettingsPage/>}></Route>
+            <Route path="login" element={<LogIn />} />
+            <Route path="signup" element={<SignUp />} />
+            <Route path="PhoneVerification" element={<PhoneVerification />} />
+            <Route path="reglages" element={<SettingsPage />}></Route>
 
+            <Route path="alerts" element={<AlertListPage />} />
+            <Route path="alerts/:id" element={<AlertInfo />} />
+            <Route path="alerts/add" element={<CreateAlert />} />
 
-              <Route path="alerts" element={<AlertListPage />} />
-              <Route path="alerts/:id" element={<AlertInfo />} />
-              <Route path="alerts/add" element={<CreateAlert />} />
+            <Route path="favoris" element={<WishlistPage />} />
+            <Route path="vitrine" element={<AnnoncePage />} />
 
-              <Route path="favoris" element={<WishlistPage/>}/>
-              <Route path="vitrine" element={<AnnoncePage/>}/>
-
-
-              <Route path="activate" element={<ActivateAccount />} />
-
-            </Routes>
-            <ScrollTopBehaviour />
-            <ChatbotFloatingButton />
+            <Route path="activate" element={<ActivateAccount />} />
+          </Routes>
+          <ScrollTopBehaviour />
+          <ChatbotFloatingButton />
         </BrowserRouter>
         <SrollTop />
       </Provider>
--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,36 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Set this to the subfolder path
+  base: '/', // Root path for Vercel deployment
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          router: ['react-router-dom'],
+          redux: ['@reduxjs/toolkit', 'react-redux'],
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@assets': path.resolve(__dirname, './public'),
     },
+  },
+  server: {
+    port: 3000,
+    host: true,
+  },
+  preview: {
+    port: 3000,
+    host: true,
+  },
+  define: {
+    global: 'globalThis',
   },
 })
